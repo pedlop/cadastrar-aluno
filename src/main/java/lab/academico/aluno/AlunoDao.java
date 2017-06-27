18d46c0d0eca915e3af0ad1796a6dc8a789bff02
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-06-27 15:26:35 -0300

    Estrutura Maven Eclipse e Apache Derby montada
    Realizar os métodos de exclusão e alteração de aluno.

---
+++ src/main/java/lab/academico/aluno/AlunoDao.java
@@ -0,0 +1,46 @@
+package lab.academico.aluno;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+/**
+ * Data Access Object.
+ */
+public class AlunoDao {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("lab-persistence-unit");
+
+	public static void inclui(String matricula, String nome) {
+		// Obter "conexão".
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Aluno aluno = new Aluno();
+		aluno.setMatricula(matricula);
+		aluno.setNome(nome);
+
+		// Grava o objeto no banco de dados.
+		em.persist(aluno);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void alterar(String matricula, String nome) {
+	}
+
+	public static void excluir(String matricula) {
+	}
+
+	public static List<Aluno> listar() {
+		EntityManager em = emf.createEntityManager();
+		String jpql = "from Aluno";
+		TypedQuery<Aluno> query = em.createQuery(jpql, Aluno.class);
+		List<Aluno> result = query.getResultList();
+		em.close();
+		return result;
+	}
+}
